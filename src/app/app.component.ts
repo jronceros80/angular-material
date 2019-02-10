5bf90b562ff3ed70fe247ef854cd7e4e8a353b5c
jronceros80 <jronceros80@gmail.com>
2019-02-10 23:19:09 +0100

    ejemplos de angular material

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isDarkTheme = false;
+  lastDialogResult: string;
+  mode: string;
+  value: number;
+  color: string;
+  progress = 0;
+
+
+  slider = {
+    'autoTicks': false,
+    'disabled': false,
+    'invert': false,
+    'max': 100,
+    'min': 0,
+    'showTicks': false,
+    'step': 1,
+    'thumbLabel': false,
+    'value': 0,
+    'vertical': false,
+    'tickInterval': 1,
+    'checked': true
+  };
+
+  colors = [
+    { id: 1, name: 'Red'},
+    { id: 2, name: 'Green'},
+    { id: 3, name: 'Blue'}
+  ];
+
+  foods: any[] = [
+    { name: 'Pizza', rating: 'Excellent' },
+    { name: 'Burritos', rating: 'Great' },
+    { name: 'French fries', rating: 'Pretty good' },
+  ];
+
+  availableColors = [
+    { name: 'none', color: '' },
+    { name: 'Primary', color: 'primary' },
+    { name: 'Accent', color: 'accent' },
+    { name: 'Warn', color: 'warn' }
+  ];
+
+  tiles = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
+  title = 'angular-material';
+}
---
+++ src/app/material.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@NgModule({
+  imports: [
+      MatButtonModule,
+      MatCheckboxModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatMenuModule,
+      MatRadioModule,
+      MatSelectModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatCardModule,
+      MatListModule,
+      MatChipsModule,
+      MatGridListModule,
+      MatSliderModule,
+      MatProgressSpinnerModule],
+  exports: [
+      MatButtonModule,
+      MatCheckboxModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatMenuModule,
+      MatRadioModule,
+      MatSelectModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatCardModule,
+      MatListModule,
+      MatChipsModule,
+      MatGridListModule,
+      MatSliderModule,
+      MatProgressSpinnerModule],
+})
+export class MaterialModule { }
